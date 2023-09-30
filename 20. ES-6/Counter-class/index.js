cfc7a15f3cc82b1ddf469c907362a790e7bc32cd
Mihir Soni <ms6711534@gmail.com>
2023-09-30 12:34:56 +0530

    Counter Class assinment done

--- 20. ES-6/Counter-class/index.js
+++ 20. ES-6/Counter-class/index.js
@@ -1,37 +1,47 @@
 let display = document.getElementById('display');
 let autoBtn = document.getElementById('autoBtn');
-let i = 0;
 let timer = null;
 
-let plus = ()=>{
-    i++;
-    display.textContent = i;
-    clearInterval(timer);
-    timer = null;
-    autoBtn.src = 'https://www.transparentpng.com/thumb/play/Sk7IcT-play-icons-free-icons-ios-icons-icon-search-engine.png'
-}
-
-let minus = ()=>{
-    i--;
-    if(i<0) i=0;
-    display.textContent = i;
-    clearInterval(timer);
-    timer = null;
-    autoBtn.src = 'https://www.transparentpng.com/thumb/play/Sk7IcT-play-icons-free-icons-ios-icons-icon-search-engine.png'
-}
-
-let auto = ()=>{
-    if(timer===null){
-        timer = setInterval(()=>{
-            i++;
-            display.textContent = i;
-        },1000)
-        autoBtn.src='https://www.freeiconspng.com/uploads/simple-green-pause-icon-3.png'
-    }
-    else{
-        clearInterval(timer)
+class Counter {
+    plus() {
+        display.textContent++;
+        console.log(display.textContent);
+        clearInterval(timer);
         timer = null;
         autoBtn.src = 'https://www.transparentpng.com/thumb/play/Sk7IcT-play-icons-free-icons-ios-icons-icon-search-engine.png'
     }
+    minus() {
+        display.textContent--;
+        if (display.textContent < 0) display.textContent = 0;
+        clearInterval(timer);
+        timer = null;
+        autoBtn.src = 'https://www.transparentpng.com/thumb/play/Sk7IcT-play-icons-free-icons-ios-icons-icon-search-engine.png'
+    }
+    auto() {
+        if (timer === null) {
+            timer = setInterval(() => {
+                display.textContent++;
+            }, 1000)
+            autoBtn.src = 'https://www.freeiconspng.com/uploads/simple-green-pause-icon-3.png'
+        }
+        else{
+            clearInterval(timer);
+            autoBtn.src = 'https://www.transparentpng.com/thumb/play/Sk7IcT-play-icons-free-icons-ios-icons-icon-search-engine.png';
+            timer = null;
+        }
+    }
 }
+
+let myCounter = new Counter();
+
+let auto = ()=>{
+    myCounter.auto()
+}
+let plus = ()=>{
+    myCounter.plus()
+}
+let minus = ()=>{
+    myCounter.minus()
+}
+
 auto()
