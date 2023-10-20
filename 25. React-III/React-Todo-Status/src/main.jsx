ee77171d1bf151b9fc3f1ccd1605971b368f8449
Mihir Soni <ms6711534@gmail.com>
2023-10-20 20:46:18 +0530

    React todo assinment done

--- 25. React-III/React-Todo-Status/src/main.jsx
+++ 25. React-III/React-Todo-Status/src/main.jsx
@@ -12,34 +12,66 @@ const App = () => {
     }
 
     const addTask = () => {
-        const newTask = {
-            id: inputValue + Math.random(),
-            title: inputValue,
-            status: false
+        if (inputValue != "") {
+
+            const newTask = {
+                id: inputValue + Math.random(),
+                title: inputValue,
+                status: false,
+                message: 'Incomplete',
+                completeMsg: 'Mark as Complete'
+            }
+            const updatedTaskList = [...taskList, newTask]
+            setTaskList(updatedTaskList)
+            setInputValue('')
         }
-        const updatedTaskList = [...taskList, newTask]
-        setTaskList(updatedTaskList)
-        setInputValue('')
     }
 
     const del = (taskId) => {
-        const newTaskList = taskList.filter(ele => taskId != ele.id)
-        setTaskList(newTaskList)
+        console.log(taskId)
+        console.log(taskList)
+        const newTaskList1 = taskList.filter(ele => taskId != ele.id)
+        setTaskList(newTaskList1)
     }
+
+    const stat = (taskId) => {
+        const newTaskList2 = taskList.map((ele) => {
+          if (taskId == ele.id) {
+            if (ele.status) {
+              return {...ele, status:false, message:"Incomplete", completeMsg:"Mark as Complete"}
+            } else {
+              return {...ele, status:true, message:"Completed", completeMsg:"Mark as InComplete"}
+            }
+          }
+          else{
+            return ele;
+          }
+        });
+        setTaskList(newTaskList2);
+      };
 
     return (
         <>
+            <div className='head'><h1>My Todo List</h1></div>
             <div className='add-task'>
                 <input id='input' placeholder='...' onChange={handleChange} value={inputValue} />
                 <button onClick={addTask}>Add Task</button>
+            </div>
+            <div className='taskItem'>
+                <div><p>Tasks</p></div>
+                <div><p>Status</p></div>
+                <div className='delDiv'>
+                    <p>Buttons</p>
+                </div>
             </div>
             <div className='task-list'>
                 {
                     taskList.map(ele => (
                         <div key={ele.id} className='taskItem'>
-                            <p>{ele.title}</p>
-                            <p>Incomplete</p>
+                            <div><p>{ele.title}</p></div>
+                            <div><p>{ele.message}</p></div>
                             <div className='delDiv'>
+                                <button onClick={() => stat(ele.id)}>{ele.completeMsg}</button>
                                 <button onClick={() => del(ele.id)}>delete</button>
                             </div>
                         </div>
